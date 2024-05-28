bd25ff31e25e96f18d9727181df4f41d0fc0ff8e
Marsel <shaidullov-marsel@mail.ru>
2024-05-29 00:07:47 +0300

    learn

--- src/index.js
+++ src/index.js
@@ -2,20 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
-const text = 'Hello World Marsel!';
-
-const elem = (
-  <div>
-    <h2 className='learn'>Текст: {text}</h2>
-    <input type="text" />
-    <button>Click</button>
-  </div>
-);
-
+import { Header } from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  elem
+  <App/>
 );
 
