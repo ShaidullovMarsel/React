cb8f54f3565389f21469af22b1425193a5700699
Marsel <shaidullov-marsel@mail.ru>
2024-05-28 23:15:56 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const text = 'Hello World!';
+const text = 'Hello World Marsel!';
 
 const elem = (
   <div>
